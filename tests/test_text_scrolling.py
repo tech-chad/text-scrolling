53c1b69683019b4172e15bcf4717c58f37554f94
Chad Larson <techchad2@gmail.com>
2022-05-25 23:17:55 -0500

    Basic Functionality

---
+++ tests/test_text_scrolling.py
@@ -0,0 +1,56 @@
+import pytest
+
+import text_scrolling
+
+
+@pytest.mark.parametrize("argument, expected_result", [
+    ([], 4), (["-s", "1"], 1), (["--speed", "9"], 9)
+])
+def test_argument_parser_speed(argument, expected_result):
+    result = text_scrolling.argument_parser(argument)
+    assert result.speed == expected_result
+
+
+@pytest.mark.parametrize("argument, expected_result", [
+    ([], "Your custom text here"), (["test string"], "test string")
+])
+def test_argument_parser_text(argument, expected_result):
+    result = text_scrolling.argument_parser(argument)
+    result.text = expected_result
+
+
+@pytest.mark.parametrize("argument, expected_speed, expected_text", [
+    ([], 4, "Your custom text here"),
+    (["test string"], 4, "test string"),
+    (["test string", "-s", "3"], 3, "test string"),
+    (["--speed", "7"], 7, "Your custom text here"),
+])
+def test_argument_parser_text_speed(argument, expected_speed, expected_text):
+    result = text_scrolling.argument_parser(argument)
+    assert result.speed == expected_speed
+    assert result.text == expected_text
+
+
+@pytest.mark.parametrize("argument, expected_result", [
+    ([], False), (["--screensaver"], True),
+])
+def test_argument_parser_screensaver(argument, expected_result):
+    result = text_scrolling.argument_parser(argument)
+    assert result.screensaver == expected_result
+
+
+@pytest.mark.parametrize("test_values, expected_results", [
+    ("0", 0), ("1", 1), ("2", 2), ("3", 3), ("4", 4),
+    ("5", 5), ("6", 6), ("7", 7), ("8", 8), ("9", 9)
+])
+def test_positive_int_zero_to_nine_normal(test_values, expected_results):
+    result = text_scrolling.positive_int_zero_to_nine(test_values)
+    assert result == expected_results
+
+
+@pytest.mark.parametrize("test_values", [
+    "-5", "10", "100", "2.5", " ", "Test", "test&*#", "", " 125445244545"
+])
+def test_positive_int_zero_to_nine_error(test_values):
+    with pytest.raises(text_scrolling.argparse.ArgumentTypeError):
+        text_scrolling.positive_int_zero_to_nine(test_values)
---
+++ text_scrolling.py
@@ -0,0 +1,106 @@
+from __future__ import annotations
+
+import argparse
+import curses
+import random
+import time
+
+from typing import Optional
+from typing import Sequence
+
+DEFAULT_TEXT = "Your custom text here"
+DEFAULT_SPEED = 4
+SPEED = [0.01, 0.025, 0.03, 0.04, 0.05, 0.07, 0.08, 0.09, 0.15, 0.3]
+
+
+def set_curses_color() -> None:
+    curses.init_pair(1, random.randrange(0, curses.COLORS), 16)
+
+
+def curses_main(screen: curses._CursesWindow, args: argparse.Namespace):
+    curses.curs_set(0)  # Set the cursor to off.
+    screen.timeout(0)  # Turn blocking off for screen.getch().
+    curses.use_default_colors()
+    set_curses_color()
+    delay = SPEED[args.speed]
+
+    size_y, size_x = screen.getmaxyx()
+    x = text_end = size_x - 1
+    y = random.randint(0, size_y - 2)
+    text_start = 0
+
+    run = True
+    while run:
+        time.sleep(delay)
+        size_y, size_x = screen.getmaxyx()  # screen resizing
+        if y >= size_y - 1 or x >= size_x - 1:
+            x = text_end = size_x - 1
+            y = random.randint(0, size_y - 2)
+            text_start = 0
+            screen.clear()
+
+        if x >= 1:
+            x -= 1
+        else:
+            x = 0
+            text_start += 1
+        text_end -= 1
+        screen.addstr(y, x,
+                      args.text[text_start:size_x - text_end],
+                      curses.color_pair(1))
+        screen.clrtoeol()  # removes the last letter
+        screen.refresh()
+        if text_start >= len(args.text) + 5:  # done with current line?
+            x = text_end = size_x - 1
+            y = random.randint(0, size_y - 1)
+            text_start = 0
+            set_curses_color()
+
+        ch = screen.getch()
+        if ch != -1 and args.screensaver:
+            run = False
+        elif ch in [81, 113]:  # q, Q
+            run = False
+        elif 48 <= ch <= 57:  # 0 to 9
+            delay = SPEED[int(chr(ch))]
+
+
+def positive_int_zero_to_nine(value: str) -> int:
+    """
+    Used with argparse.
+    Checks to see if value is positive int between 0 and 10.
+    """
+    try:
+        int_value = int(value)
+        if int_value < 0 or int_value >= 10:
+            raise argparse.ArgumentTypeError(f"{value} is an invalid positive "
+                                             f"int value 0 to 9")
+        return int_value
+    except ValueError:
+        raise argparse.ArgumentTypeError(f"{value} is an invalid positive int "
+                                         f"value 0 to 9")
+
+
+def argument_parser(argv: Optional[Sequence[str]] = None) -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("text", default=DEFAULT_TEXT, nargs="?",
+                        help="Custom text string wrapped in quotes")
+    parser.add_argument("-s", "--speed",
+                        type=positive_int_zero_to_nine,
+                        default=DEFAULT_SPEED,
+                        help=f"Set scrolling speed 0 to 9.  (0-Fast, "
+                             f"{DEFAULT_SPEED}-Default, 9-Slow)")
+
+    parser.add_argument("--screensaver", action="store_true",
+                        help="Screensaver mode. Any key will exit")
+
+    return parser.parse_args(argv)
+
+
+def main():
+    args = argument_parser()
+    curses.wrapper(curses_main, args)
+
+
+if __name__ == "__main__":
+    main()
